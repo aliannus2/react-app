4ff90ee62615798b708a694d8a03b55dd607fc4e
aliannus2 <58392315+aliannus2@users.noreply.github.com>
2021-08-25 19:00:00 +0500

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,11 +5,9 @@
 #stdout logs of this process executing can be found in /opt/codedeploy-agent/deployment-root/47../<deployment_id>/logs/scripts.log
 
 # here we update the server and install node and npm
-echo installing dependencies
 sudo apt-get update
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install nodejs -y
 sudo apt-get -y install npm
 
-cd /var/www/
 sudo npm install
