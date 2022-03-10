3609fc79c489f51b6fdeb7e9253b7ac035bb12ee
Annas Ali <zain.abideen@tkxel.io>
2022-03-10 14:46:17 +0500

    code coverage testing

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -1,8 +1,8 @@
 const scanner = require('sonarqube-scanner');
 scanner(
     {
-        serverUrl: "http://10.0.5.215:9000",
-        token: "ee3d4f2cd90a7abc8b0612f3658cd64e99ade689",
+        serverUrl: "https://tkxel-team.com/",
+        token: "295036230808088aa349648f95754477018cc75f",
         options: {
             "sonar.sources": "./src"
         },
